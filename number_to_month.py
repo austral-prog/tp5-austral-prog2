44e78df67ab106399f3599e0ae08e0fa38de789d
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-12 11:42:45 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,27 @@
-# Replace the "ANSWER HERE" for your answer
-
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    if 1 > month > 12:
+        return "error"
+    if month == 1:
+        return "enero"
+    if month == 2:
+        return "febrero"
+    if month == 3:
+        return "marzo"
+    if month == 4:
+        return "abril"
+    if month == 5:
+        return "mayo"
+    if month == 6:
+        return "junio"
+    if month == 7:
+        return "julio"
+    if month == 8:
+        return "agosto"
+    if month == 9:
+        return "septiembre"
+    if month == 10:
+        return "octubre"
+    if month == 11:
+        return "noviembre"
+    if month == 12:
+        return "diciembre"
