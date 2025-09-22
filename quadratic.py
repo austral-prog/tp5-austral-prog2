05ed8fdb2654c9be12ac1f40a3932100ee1bec20
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-22 14:16:46 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -18,13 +18,13 @@ def value_y(a, b, c, x):
 
 def to_string(a, b, c):
     if a == 0:
-        return f"f(x) = {b}*x + {c}"
+        return f"f(x) = {b} * X + {c}"
     if b == 0:
-        return f"f(x) = {a}*x^2 + {c}"
+        return f"f(x) = {a} * X^2 + {c}"
     if (a == 0) and (b == 0):
         return f"f(x) = {c}"
     else:
-        return f"f(x) = {a}*x^2 + {b}*x + {c}"
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 
 def derivation(a, b, c):
