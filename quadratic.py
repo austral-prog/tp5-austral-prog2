be7e08f6628648908dd9d007c069deba0a5316d7
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-22 13:56:17 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,26 @@
-# Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
-    return "ANSWER HERE"
+    discriminante = b**2 - 4*a*c
+    if discriminante > 0:
+        raiz_cuadrada = discriminante**0.5
+        raiz_1 = (-b + raiz_cuadrada) / (2*a)
+        raiz_2 = (-b - raiz_cuadrada) / (2*a)
+        return f"({raiz_1}, {raiz_2})"
+    if discriminante == 0:
+        raiz = -b / (2*a)
+        return f"({raiz})"
+    else:
+        return "( )"
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    return a*x**2 + b*x + c
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    return f"f(x) = {a}*x^2 + {b}*x + {c}"
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    if (c > 0) or (c < 0):
+        c = 0
+    return f"f'(x) = {2*a}*x + {b} + {c}"
